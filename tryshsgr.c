14bb6a9fa1558cecb5d78dd8f6a76c710ea9b0ab
Joern Dreyer <j.dreyer@hzdr.de>
2024-09-16 15:07:13 +0200

    Used space instead of tabs for identation.

--- tryshsgr.c
+++ tryshsgr.c
@@ -7,19 +7,19 @@
 
 int main()
 {
-	gid_t *x;
-	long ngroups_max;
+  gid_t *x;
+  long ngroups_max;
   
-	ngroups_max = sysconf(_SC_NGROUPS_MAX) + 1;
-	x = (gid_t *)malloc(ngroups_max *sizeof(gid_t));
+  ngroups_max = sysconf(_SC_NGROUPS_MAX) + 1;
+  x = (gid_t *)malloc(ngroups_max *sizeof(gid_t));
   
-    x[0] = x[1] = 1;
-    if (getgroups(ngroups_max,x) == 0) if (setgroups(ngroups_max,x) == -1) _exit(1);
-  
-    if (getgroups(ngroups_max,x) == -1) _exit(1);
-    if (x[1] != 1) _exit(1);
-    x[1] = 2;
-    if (getgroups(ngroups_max,x) == -1) _exit(1);
-    if (x[1] != 2) _exit(1);
-    _exit(0);
+  x[0] = x[1] = 1;
+  if (getgroups(ngroups_max,x) == 0) if (setgroups(ngroups_max,x) == -1) _exit(1);
+
+  if (getgroups(ngroups_max,x) == -1) _exit(1);
+  if (x[1] != 1) _exit(1);
+  x[1] = 2;
+  if (getgroups(ngroups_max,x) == -1) _exit(1);
+  if (x[1] != 2) _exit(1);
+  _exit(0);
 }
